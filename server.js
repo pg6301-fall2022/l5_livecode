8b74ef1e19a7b94748a9efb76bb8da939cda08bf
Bogdan Marculescu <marculescu.bogdan@gmail.com>
2022-09-26 09:43:19 +0200

    Commit 3 - basic POST

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ import express from "express";
 
 const app = express();
 
+app.get("/login", (req, res) => {
+    res.json({
+        username: "admin"
+    });
+});
+
+app.post("/login", (req, res) =>{
+    // POST sends data client -> server
+    console.log("Test");
+    res.sendStatus(200);
+});
+
 const server = app.listen(
      process.env.PORT || 3000,
     () => {

f2a8dc09d514451656360c4c999d7c8a4264facc
Bogdan Marculescu <marculescu.bogdan@gmail.com>
2022-10-03 10:07:31 +0200

    Step 11 - Redirect

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(bodyParser.urlencoded({
     extended:false
 }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(loginMiddleware);
 
 app.get("/login", (req, res) => {
 
@@ -32,6 +33,11 @@ const USERS = [
         username: "admin",
         password: "NOTsecret",
         fullname: "Testsson"
+    },
+    {
+        username: "otheruser",
+        password: "secret?",
+        fullname: "Some Other"
     }
 ];
 
@@ -39,32 +45,48 @@ app.post("/login", (req, res) =>{
     // POST sends data client -> server
     // somewhere here - I know that there is a request with info in the body
 
-    console.log("getting this far");
-
     const { username, password } = req.body;
 
     const user = USERS.find(u => u.username === username);
 
     if(user && user.password === password){
         res.cookie("username", username, {signed : true});
-        res.sendStatus(200);
+        res.sendStatus(200)
+            .redirect("/");
     } else {
-        res.sendStatus(401);
+        res.sendStatus(401)
+            .redirect("/");
     }
 
 
+});
 
+app.get("/users", (req, res) => {
+    if(!res.user){
+        return res.sendStatus(403);
+    }
+    res.json(USERS);
+});
+
+app.post("/users", (req, res) => {
+    const {username, password, fullname} = req.body;
+    if(!username || !password || !fullname){
+        return res.sendStatus(400);
+    }
+    USERS.push({username: username, password: password, fullname: fullname});
+
+    res.redirect("/");
 
 });
 
 app.use(express.static("public"));
 
 function loginMiddleware(req, res, next) {
-    res.user = USERS.find(u => u.username === req.username);
+    res.user = USERS.find(u => u.username === req.signedCookies.username);
     next();
 }
 
-app.use(loginMiddleware);
+
 
 const server = app.listen(
      process.env.PORT || 3000,

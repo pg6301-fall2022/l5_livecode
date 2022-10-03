88fe86c4e091cdf0b94edbbb74087f7548d1f6a7
Bogdan Marculescu <marculescu.bogdan@gmail.com>
2022-10-03 08:47:41 +0200

    Step 9 - Secret cookies no longer crash

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 app.use(bodyParser.json());
